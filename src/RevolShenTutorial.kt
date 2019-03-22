b437c468c49655fdc3536be4616ea4da0e8ae140
kohrd <konrad.janeczek@protonmail.com>
2019-03-22 14:16:20 +0100

    3

--- src/RevolShenTutorial.kt
+++ src/RevolShenTutorial.kt
@@ -1,14 +1,31 @@
+import jdk.nashorn.internal.lookup.Lookup
+
 fun main(args: Array<String>) {
+//2
 
-    var zmienna1: String = "Konrad ";
-    var zmienna2: String;
-    zmienna2 = "moje nazwisko";
-
-    var zmienna3 = "kotlin";
+//    var zmienna1: String = "Konrad ";
+//    var zmienna2: String;
+//    zmienna2 = "moje nazwisko";
+//
+//    var zmienna3 = "kotlin";
     var zmienna4 = 4;
     var wiek: Int = 37;
-    print("witaj $zmienna1, masz  $wiek lat to troche duzo "); // $ dolar oznacza zmienna
-    print("mozna tez tal ${zmienna2}") // lub dolar i klamra
+    var long1: Long = 234
+//    print("witaj $zmienna1, masz  $wiek lat to troche duzo "); // $ dolar oznacza zmienna
+//    print("mozna tez tal ${zmienna2}") // lub dolar i klamra
+    var suma: Int = zmienna4 + wiek;
+    println (suma)
+    println(long1)
+
+    var czynnik1: Int = 4
+    var czynnik2: Int = 324
+
+    var iloczyn = czynnik1 * czynnik2
+    print("iloczyn $iloczyn")
+
+    println(2+2*2)
+    println("sdsd")
+    println((2+2)*2)
 
 
 
