0417334c68d06390d2e349629e9159fc9d23d493
kohrd <konrad.janeczek@protonmail.com>
2019-03-22 13:54:00 +0100

    2

--- src/RevolShenTutorial.kt
+++ src/RevolShenTutorial.kt
@@ -1,6 +1,17 @@
 fun main(args: Array<String>) {
 
-    var Konrad: String = "moje imie";
+    var zmienna1: String = "Konrad ";
+    var zmienna2: String;
+    zmienna2 = "moje nazwisko";
+
+    var zmienna3 = "kotlin";
+    var zmienna4 = 4;
+    var wiek: Int = 37;
+    print("witaj $zmienna1, masz  $wiek lat to troche duzo "); // $ dolar oznacza zmienna
+    print("mozna tez tal ${zmienna2}") // lub dolar i klamra
+
+
+
 }
 
 
